5035212740be5a22cfdaa56c487c6ea2dea58f14
saadd28 <saadd2806@gmail.com>
2024-06-11 05:57:36 +0500

    APIs updated for deployed backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,57 +1,57 @@
 import axios from "axios";
 
 export const gettopproducts = () => {
-  return axios.get("https://adaptimart-backend.vercel.app//api/product/getall");
+  return axios.get("https://adaptimart-backend.vercel.app/api/product/getall");
 };
 
 export const getproductsbyname = (ProductName) => {
-  const url = `https://adaptimart-backend.vercel.app//api/product/getbyname?name=${ProductName}`;
+  const url = `https://adaptimart-backend.vercel.app/api/product/getbyname?name=${ProductName}`;
 
   return axios.get(url);
 };
 
 export const getcategories = () => {
-  return axios.get("https://adaptimart-backend.vercel.app//api/category/getall");
+  return axios.get("https://adaptimart-backend.vercel.app/api/category/getall");
 };
 
 export const getcategoriesbyname = (CategoryName) => {
-  const url = `https://adaptimart-backend.vercel.app//api/category/getbyname?name=${CategoryName}`;
+  const url = `https://adaptimart-backend.vercel.app/api/category/getbyname?name=${CategoryName}`;
 
   return axios.get(url);
 };
 
 export const adduser = (data) => {
-  return axios.post("https://adaptimart-backend.vercel.app//api/account/add", data);
+  return axios.post("https://adaptimart-backend.vercel.app/api/account/add", data);
 };
 
 export const addorder = (data) => {
-  return axios.post("https://adaptimart-backend.vercel.app//api/order/add", data);
+  return axios.post("https://adaptimart-backend.vercel.app/api/order/add", data);
 };
 
 export const getreviewratingandtotal = (ProductId) => {
-  const url = `https://adaptimart-backend.vercel.app//api/review/gettotal?product_id=${ProductId}`;
+  const url = `https://adaptimart-backend.vercel.app/api/review/gettotal?product_id=${ProductId}`;
 
   return axios.get(url);
 };
 
 export const getreviewslistbyproductid = (ProductId) => {
-  const url = `https://adaptimart-backend.vercel.app//api/review/getbypid?product_id=${ProductId}`;
+  const url = `https://adaptimart-backend.vercel.app/api/review/getbypid?product_id=${ProductId}`;
 
   return axios.get(url);
 };
 
 export const addreview = (data) => {
-  return axios.post("https://adaptimart-backend.vercel.app//api/review/add", data);
+  return axios.post("https://adaptimart-backend.vercel.app/api/review/add", data);
 };
 
 export const login = (Email, Password) => {
-  const url = `https://adaptimart-backend.vercel.app//api/account/login?email=${Email}&password=${Password}`;
+  const url = `https://adaptimart-backend.vercel.app/api/account/login?email=${Email}&password=${Password}`;
 
   return axios.get(url);
 };
 
 export const getcouponbycode = (Code) => {
-  const url = `https://adaptimart-backend.vercel.app//api/coupon/getbycode?code=${Code}`;
+  const url = `https://adaptimart-backend.vercel.app/api/coupon/getbycode?code=${Code}`;
 
   return axios.get(url);
 };
@@ -64,7 +64,7 @@ export const getmbi = (data) => {
 };
 
 export const getproductbyid = (Id) => {
-  const url = `https://adaptimart-backend.vercel.app//api/product/getbyid?id=${Id}`;
+  const url = `https://adaptimart-backend.vercel.app/api/product/getbyid?id=${Id}`;
 
   return axios.get(url);
 };
